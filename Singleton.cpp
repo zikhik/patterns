575b0c3e055b86fa1e5e18fcae29ccd0f1635f3f
zikhik <slavyanskiji@gmail.com>
2016-12-16 14:49:59 +0500

    Update Singleton.cpp

--- Singleton.cpp
+++ Singleton.cpp
@@ -1,13 +1,18 @@
-class OnlyOne
+class Singleton
 {
-public:
-        static const OnlyOne& Instance()
-        {
-                static OnlyOne theSingleInstance;
-                return theSingleInstance;
-        }
-private:        
-        OnlyOne(){}
-        OnlyOne(const OnlyOne& root);
-        OnlyOne& operator=(const OnlyOne&);
+  public:
+    static Singleton& Instance()
+    {
+        // согласно стандарту, этот код  потокобезопасный
+        static Singleton s;
+        return s;
+    }
+
+  private:
+    Singleton() { ... }  // конструктор недоступен
+    ~Singleton() { ... } // и деструктор
+
+    // необходимо также запретить копирование
+    Singleton(Singleton const&) = delete; // реализация не нужна
+    Singleton& operator= (Singleton const&) = delete;  // и тут
 };
